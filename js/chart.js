a33f5d33c64cd3037f14178c3b9de416afb95280
angelalonsopazruiz <angel.paz.consultas@gmail.com>
2024-05-27 20:47:45 -0500

    se añade parámetros de graficos chart

--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,65 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+
+const state = new AppState();
+state.loadItems();
+
 function renderChart() {
+
+    const ctx = canvasElem.getContext("2d");
+    const productNames = state.allProducts.map(product => product.name);
+    const productVotes = state.allProducts.map(product => product.timesClicked);
+    const productShown = state.allProducts.map(product => product.timesShown);
+    console.log(productNames);
+    console.log(productVotes);
+    console.log(productShown);
+
+
+    
+    chartInstance = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: productNames,
+            datasets: [{
+                label: 'clicks',
+                data: productVotes,
+                borderWidth: 1,
+                backgroundColor: "#8bc2f6",
+        
+            },
+            {
+                label: 'views',
+                data: productShown,
+                borderWidth: 1,
+                backgroundColor: "#83ea3a",
+            }]
+        },
+        options: {
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: "black"
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: "black"
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    labels: {
+                        color: "black"
+                    }
+                }
+            }
+        }
+    }
+    );
 }
 
 renderChart();
